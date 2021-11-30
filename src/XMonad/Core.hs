fa3926efabf5dfe39de553a4d89a43296ed7c996
Michael Sloan <mgsloan@gmail.com>
2021-11-29 20:20:07 -0700

    Expose X constructor

--- src/XMonad/Core.hs
+++ src/XMonad/Core.hs
@@ -26,7 +26,7 @@
 -----------------------------------------------------------------------------
 
 module XMonad.Core (
-    X, WindowSet, WindowSpace, WorkspaceId,
+    X(..), WindowSet, WindowSpace, WorkspaceId,
     ScreenId(..), ScreenDetail(..), XState(..),
     XConf(..), XConfig(..), LayoutClass(..),
     Layout(..), readsLayout, Typeable, Message,
