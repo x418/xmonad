3c510f891e6b9ee52282eb4c4e3d21e09d50fe8d
Michael Sloan <mgsloan@gmail.com>
2024-01-07 18:49:14 -0700

    Expose sendRestart function

--- src/XMonad/Main.hs
+++ src/XMonad/Main.hs
@@ -15,7 +15,7 @@
 --
 -----------------------------------------------------------------------------
 
-module XMonad.Main (xmonad, buildLaunch, launch) where
+module XMonad.Main (xmonad, launch, sendRestart) where
 
 import System.Locale.SetLocale
 import qualified Control.Exception as E
